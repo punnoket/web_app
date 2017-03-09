bdcd097f3473f582380e999290f9dfc0a75ee432
Punnnnn ' n <Punnnnn ' n>
2017-03-10 02:00:39 +0700

    parse json

--- app.js
+++ app.js
@@ -2,28 +2,43 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require("path");
+//var question = require('./question');
+var fs = require('fs');
+var obj;
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+
+fs.readFile('./question.json', 'utf8', function (err, data) {
+  if (err) throw err;
+  question = JSON.parse(data);
+});
+
 app.get('/', function getHomePage(req, res) {
     res.sendFile(__dirname + '/views/home.html');
 });
 
-app.get('/tutorial', function getHomePage(req, res) {
-      res.sendFile(__dirname + '/views/tutorial.html');
+app.get('/tutorial_game', function getAboutPage(req, res) {
+    res.sendFile(__dirname + '/views/tutorial_game.html');
 });
-
 
 app.get('/game', function getAboutPage(req, res) {
-    res.sendFile(__dirname + '/views/start_game.html');
+    res.sendFile(__dirname + '/views/game.html');
 });
 
-app.get('/point', function getPoint(req, res) {
-    var body = req.body;
+app.post('/point', function getPoint(req, res) {
+    var body = req.body.answer;
     console.log(body);
+});
+
+app.post('/question', function (req, res) {
+    console.log(question.detail);
+    res.end(JSON.stringify(question));
+
 });
 
 var server = app.listen(8080, function() {
